0a00677978ec8360db4ddf7ac007c79d355ebb17
jamauro <jamauro@gmail.com>
2014-05-18 18:34:01 -0500

    Quetzal Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quetzal::Application.config.secret_key_base = '7595462f63621d032d0072d3b8f3e9b367fa7574896637ec67ef16ae10a23fba15b1c1e62b0280ba1b8439eb995c85226ada3e00e93d9ce613cb22e5fe0e8ff1'
